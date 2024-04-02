eb28e8edf375e97df5b11f3361eb000d9b23b44d
Darshan <darshn001@gmail.com>
2024-04-02 18:35:50 +0530

    Add java code of For Loop

---
+++ basics/ForLoop.java
@@ -0,0 +1,27 @@
+package forLoop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<8;i++) {
+			System.out.println("Formula 1");
+		}
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+	}
+		int[] darshan=new int[4];
+		darshan[0]=3;
+		darshan[1]=2;
+		darshan[2]=32;
+		darshan[3]=11;
+		for(int i=0;i<4;i++) {
+			System.out.println(darshan[i]);
+		}
+		int dar[]= {3,4,2};
+		for(int i=0;i<dar.length;i++) {
+			System.out.println(dar[i]);
+		}
+
+}
+}
